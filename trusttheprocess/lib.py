77ee70e89bf8a75af9b5516b37b89c36a00b667e
Olaf Winkler <olaf.winkler@mein.gmx>
2022-05-23 16:18:57 +0200

    changes

--- trusttheprocess/lib.py
+++ trusttheprocess/lib.py
@@ -1,2 +1,11 @@
+import plotly.express as px
+
+def first_plot():
+    df = px.data.gapminder().query("country=='Canada'")
+    fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+    fig.show()
+    return fig
+
+
 def try_me():
-    return 'Trust THE PROCESS!!'
+    return 'TRUST THE PROCESS!! - Learn to trust, git ur hub!'
